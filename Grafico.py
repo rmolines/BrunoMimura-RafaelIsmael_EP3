75bcc2a9e4c0fa2f7908dafcdb6feb70a8fd99cd
bhmimura <bhmimura@gmail.com>
2015-04-20 17:24:07 -0300

    Update Grafico.py

--- Grafico.py
+++ Grafico.py
@@ -7,25 +7,40 @@ Created on Mon Apr 20 11:49:13 2015
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
-def Grafico (X,Y1,Y2,eixoX,eixoY1,eixoY2):
+def Grafico (Y1,Y2,eixoX,eixoY1,eixoY2, datas):
+    index = np.arange(len(Y1))
     
     fig, ax1 = plt.subplots()
-    ax1.plot(X, Y1, 'b' + '-')
-    ax1.set_xlabel(eixoX)
-    ax1.set_ylabel(eixoY1, color='b')
-    for tl in ax1.get_yticklabels():
-        tl.set_color('b')
-        
-    ax2 = ax1.twinx()
-    ax2.plot(X, Y2, 'r' + '-')
-    ax2.set_ylabel(eixoY2, color='r')
-    for tl in ax2.get_yticklabels():
-        tl.set_color('r')
-    plt.show() 
     
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.gcf().autofmt_xdate()
+    plt.bar(index, Y1, 0.35, color='b', label=eixoY1)
+    
+    plt.bar(index + 0.35, Y2, 0.35, color='r', label=eixoY2)
+    
+    plt.xlabel(eixoX)
+    plt.ylabel('Calorias')
+    plt.title('Calorias diarias')
+    plt.xticks(index + 0.35, (datas))
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
+
+def Grafico2 (Y,eixoX, eixoY, datas, x, y):
+    index = np.arange(len(Y))
+    
+    fig, ax1 = plt.subplots()
+    
+    plt.bar (index+0.35, Y, 0.35, color='b', label=eixoY)
+
+    plt.xlabel(eixoX)
+    plt.ylabel(x)
+    plt.title(x+y)
+    plt.xticks(index+0.53, (datas))
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
     
     return None
